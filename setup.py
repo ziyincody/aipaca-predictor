b5c4c84b05ceef19f4fa2a3353b2bc940d031f1d
Cody Wang <codywang@codys-mbp.lan>
2021-02-01 19:33:42 -0500

    setup precommit, gitignore, setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.1"
+DESCRIPTION = "Aipaca Model Time Predictor"
+LONG_DESCRIPTION = "Easily predict model training time (Currently Keras Only)"
+
+# Setting up
+setup(
+    # the name must match the folder name 'verysimplemodule'
+    name="aipaca_predictor",
+    version=VERSION,
+    author="Cody Wang",
+    author_email="codyw@aipaca-corp.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],  # add any additional packages that
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.6",
+)
